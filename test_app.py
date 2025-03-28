8f6ea4c34b1434513a3fc9c43f430bcf9fbd84d9
Sanich <sforwonet@gmail.com>
2025-03-28 23:34:46 +0100

    task 2

---
+++ test_app.py
@@ -0,0 +1,24 @@
+from myapp.app import add, subtract, generate_random_value, jakobi_method
+
+
+def test_add():
+
+    assert add(2, 3) == 5
+
+def test_subtract():
+
+    assert subtract(2, 3) == -1
+
+def test_random_value():
+
+    assert generate_random_value() == True
+
+def test_jakubi_method():
+
+    assert jakobi_method(A=[[1, 2], [3, 4]], b=[5, 6]) == [1, 2]
+
+
+test_add()
+test_subtract()
+test_random_value()
+test_jakubi_method()
